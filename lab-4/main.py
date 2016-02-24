f1ffa737876b95933a314dab2d160d4902c50135
Ahmed Abdel-wahab <ahmed.abdel-wahab@robustastudio.com>
2016-02-25 00:29:58 +0200

    fixes

--- lab-4/main.py
+++ lab-4/main.py
@@ -27,7 +27,7 @@ def remove_immediat_recursion(rule_head, tail):
   for term in tail[::]:  # assume rule_head = A
     most_left_symbol = term[0]
     if rule_head == most_left_symbol[0]:  # found A -> Axxxx
-      tail.pop(0)  # remove Axxxx
+      tail.remove(term)  # remove Axxxx
       term.remove(most_left_symbol)  # remove A
       prime_tail.append(term)  # move xxxx to A'
     term.append((prime_rule_head, True))  # add A' whether it's the recursion one or not
@@ -44,8 +44,6 @@ def contains_immediate_recursion(rule_head, tail):
 
 
 def replace_occurences(rule_to_process, rule_to_replace):  # E -> Ax, A -> Edf|xe
-  # print('--', represent_rule(rule_to_process[0], rule_to_process[1]))
-  # print('--', represent_rule(rule_to_replace[0], rule_to_replace[1]))
   for main_term in rule_to_process[1][::]:
     for symbol in main_term[::]:
       if symbol[0] == rule_to_replace[0]:
@@ -54,20 +52,31 @@ def replace_occurences(rule_to_process, rule_to_replace):  # E -> Ax, A -> Edf|x
         rule_to_process[1].remove(main_term)
         for replace_term in rule_to_replace[1][::]:
           temp_term = main_term[::]
-          for replace_symbol in replace_term:
+          for replace_symbol in reversed(replace_term):
             temp_term.insert(replacement_index, replace_symbol)
+          # print(represent_rule(rule_to_replace[0], [temp_term]))
+          print(represent_rule(rule_to_process[0], [temp_term]))
+          print('------')
           rule_to_process[1].append(temp_term)
 
 
 def remove_indirect_recursion(heads=[], rules={}):
+  print_rules(rules)
+  print('i ------')
   for i in range(len(heads)):
     rule_to_process = (heads[i], rules[heads[i]])
     for j in range(i):
       rule_to_replace = (heads[j], rules[heads[j]])
       replace_occurences(rule_to_process, rule_to_replace)
+    print_rules(rules)
+    print('before direct ------')
+    print('--- has recursion -- ', represent_rule(rule_to_process[0], rule_to_process[1]))
     if contains_immediate_recursion(rule_to_process[0], rule_to_process[1]):
+      print('=== yes')
       prime_rule = remove_immediat_recursion(rule_to_process[0], rule_to_process[1])
       rules[prime_rule[0]] = prime_rule[1]
+    print_rules(rules)
+    print('after direct --------')
 
 
 # def remove_recursion(rules):
@@ -79,12 +88,14 @@ def remove_indirect_recursion(heads=[], rules={}):
 #   rules.update(prime_rules)
 
 if __name__ == '__main__':
-  file_name = 'Sample3.in'
-  lines, rules = parse(file_name)
-  heads = []
-  for i in range(0, len(lines), 2):
-    heads.append(lines[i])
-  # print('--', heads)
-  remove_indirect_recursion(heads, rules)
-  # print_rules(rules)
-  write_output_file(file_name, rules, heads)
+  file_names = ['Sample2.in', 'Sample2.in', 'Sample3.in']
+  for file_name in file_names:
+    lines, rules = parse(file_name)
+    heads = []
+    for i in range(0, len(lines), 2):
+      heads.append(lines[i])
+    # print('--', heads)
+    remove_indirect_recursion(heads, rules)
+    # print_rules(rules)
+    write_output_file(file_name, rules, heads)
+    print('##############')
